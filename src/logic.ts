05b534b237bf63d8e5cce267ea418f7872f07caf
Matt Wilson <mawilson2012@gmail.com>
2022-02-27 13:58:24 -0800

    Fixed Voronoi predator logic Fixed Voronoi predator logic when choosing a bad move to make a prey's move worse Added bonus predator bonus when limiting prey to 0 moves

--- src/logic.ts
+++ src/logic.ts
@@ -1,4 +1,4 @@
-export const version: string = "1.3.5" // need to declare this before imports since several imports utilize it
+export const version: string = "1.3.6" // need to declare this before imports since several imports utilize it
 
 import { evaluationsForMachineLearning } from "./index"
 import { InfoResponse, GameState, MoveResponse } from "./types"
