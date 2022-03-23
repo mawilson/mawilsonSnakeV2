671ec294e8dddd5a30b40d93da467c3930ce4ca7
Matt Wilson <mawilson2012@gmail.com>
2022-03-22 18:10:17 -0700

    Restored prior smaller tailOffset penalties of .3-.1

--- src/logic.ts
+++ src/logic.ts
@@ -1,4 +1,4 @@
-export const version: string = "1.3.24" // need to declare this before imports since several imports utilize it
+export const version: string = "1.3.25" // need to declare this before imports since several imports utilize it
 
 import { evaluationsForMachineLearning } from "./index"
 import { InfoResponse, GameState, MoveResponse } from "./types"
