8f3f37c8ab9e258b25b4ad9d401ca6e5d11d73a0
Matt Wilson <mawilson2012@gmail.com>
2022-08-11 13:20:57 -0700

    Removed flipFlop from minimax evaluations

--- src/eval.ts
+++ src/eval.ts
@@ -948,7 +948,7 @@ export function evaluate(gameState: GameState, _myself: Battlesnake, _priorKissS
     evaluationResult.center = xDiff * evalSoloCenter + yDiff * evalSoloCenter
   }
 
-  if (isWrapped) { 
+  if (isWrapped && !isMinimaxDuel) { // metric is useful outside of duel but minimax is smarter than it in duel 
     if (haveWon) { // don't penalize snake for winning
       evaluationResult.flipFlop = evalWrappedFlipFlopStep
     } else if (originalTurn > 1) { // ignore this on early turns, just get starting food
